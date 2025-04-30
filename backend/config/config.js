aeac582e320f0c2e0f9fd5b68529580026ad67af
Pierre <galarretajeanpierre@gmail.com>
2025-04-30 17:07:08 -0500

    Config arreglado

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,13 @@
-const GOOGLE_URL = 'http://localhost:3001/api/auth/google/callback';
-const GITHUB_URL = 'http://localhost:3001/api/auth/github/callback';
-const LOGIN_URL = 'http://localhost:3001/api/auth/login';
-const REGISTER_URL = 'http://localhost:3001/api/auth/register';
-const VERIFY_URL = 'http://localhost:3001/api/auth/verify'; // Ruta protegida
+const GOOGLE_URL = 'https://blog-backend-upk0.onrender.com/api/auth/google/callback';
+const GITHUB_URL = 'https://blog-backend-upk0.onrender.com/api/auth/github/callback';
+const LOGIN_URL = 'https://blog-backend-upk0.onrender.com/api/auth/login';
+const REGISTER_URL = 'https://blog-backend-upk0.onrender.com/api/auth/register';
+const VERIFY_URL = 'https://blog-backend-upk0.onrender.com/api/auth/verify'; // Ruta protegida
 
 
-const DASHBOARD_URL = 'http://localhost:5173/dashboard'; // URL del frontend para redirigir después del login exitoso
-const LOGIN_REDIRECT_URL = 'http://localhost:5173/login'; // URL del frontend para redirigir después de un fallo en el login
-const FRONTEND_URL = 'http://localhost:5173'
+const DASHBOARD_URL = 'https://blog-frontend-wzze.onrender.com/dashboard'; // URL del frontend para redirigir después del login exitoso
+const LOGIN_REDIRECT_URL = 'https://blog-frontend-wzze.onrender.com/login'; // URL del frontend para redirigir después de un fallo en el login
+const FRONTEND_URL = 'https://blog-frontend-wzze.onrender.com'
 
 
 module.exports = { GOOGLE_URL, GITHUB_URL, LOGIN_URL, REGISTER_URL, VERIFY_URL, DASHBOARD_URL, LOGIN_REDIRECT_URL, FRONTEND_URL };
